a0472c968f04cf6e4ade26005182c215c6dffd66
DanneMite <danieledsinger@hotmail.com>
2015-02-18 18:46:03 +0100

    Zone 0.3.1 now working with scene

--- src/gui/Zone.java
+++ src/gui/Zone.java
@@ -1,26 +1,31 @@
 package gui;
-
 import core.EventAdder;
-import core.MouseObject;
+import core.GlobalStateManager;
 
 import java.awt.Image;
-import java.util.HashMap;
-import java.util.List;
+
+import combat.Enemy;
 
 /**
- * @author		Daniel Edsinger 	<danieledsinger@hotmail.com>
- * @version		0.3
- * @since		2015-02-17
- */
-
+* @author	Daniel Edsinger <danieledsinger@hotmail.com>
+* @version	0.3.1
+* @since	2015-02-18
+*/
 public class Zone extends ZButton
 {
-	
 	private String zoneName;
+	private Enemy enemy;
 	
 	public Zone(EventAdder eventAdder, String eventOnClick, Image image, int x, int y, String name){
 		super(eventAdder, eventOnClick, image, x, y);
-		zoneName = name;
+		this.zoneName = name;
+		this.enemy = enemy;
+		GlobalStateManager.getInstance().updateWorldState(zoneName, "");
+	}
+	
+	public String getname()
+	{
+		return zoneName;
 	}
 
 }
