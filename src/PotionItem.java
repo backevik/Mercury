9993ce68fae2a2022bd622bdb4b4e0ac88053f03
DanneMite <danieledsinger@hotmail.com>
2015-02-18 19:08:34 +0100

    PotionItem 0.3

---
+++ src/PotionItem.java
@@ -0,0 +1,27 @@
+package player;
+
+import java.io.Serializable;
+
+import character.Item;
+
+/**
+* @author		Daniel Edsinger 	<danieledsinger@hotmail.com>
+* @version		0.3
+* @since		2015-02-17
+*/
+public class PotionItem extends Item implements Serializable{
+	
+	private static final long serialVersionUID = 3631163768021633820L;
+	int healPower;
+	
+	public PotionItem()
+	{
+		super("Potion", "Heal for 10", 5, 9, 1);
+		healPower = 10;
+	}
+	
+	@Override
+	public void use() {
+		
+	}
+}
