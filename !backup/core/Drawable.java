e0d07c7236fa6a4307bfbcc5354adfb59e4d3a41
Mattias Benngard <mbengan@gmail.com>
2015-02-18 12:03:51 +0100

    Removed files from Core

--- !backup/core/Drawable.java
+++
@@ -1,8 +0,0 @@
-package core;
-
-import java.awt.Graphics;
-
-public interface Drawable
-{
-	public abstract void render (Graphics g);
-}
--- !backup/core/MouseObject.java
+++
@@ -1,6 +0,0 @@
-package core;
-
-public interface MouseObject
-{
-	public void onClick (int mouseX, int mouseY);
-}
