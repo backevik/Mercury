1d255dcd43755e9d714c14ed30e1e8b63663541c
DanneMite <danieledsinger@hotmail.com>
2015-02-18 14:11:20 +0100

    ImageDatabase 0.3.1 maps in maps
    Added support for maps within maps.

--- src/database/ImageDatabase.java
+++ src/database/ImageDatabase.java
@@ -4,36 +4,60 @@ import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
+
 import javax.imageio.ImageIO;
 
 /**
  * @author		Daniel Edsinger 	<danieledsinger@hotmail.com>
- * @version		0.3
- * @since		2015-02-17
- */	
+ * @version		0.3.1
+ * @since		2015-02-18
+ */
+	
 
 public class ImageDatabase
 {
 	private final HashMap<String, Image> images = new HashMap<String, Image>();
 	
-	public ImageDatabase() {		
-		initialize();
+	public ImageDatabase(){
+		
+		File baseDirectory = new File("img/");	//base dir
+		File[] f = baseDirectory.listFiles();	//List of files and maps in base dir
+		
+		if(baseDirectory.isDirectory()) {
+			
+			for(File file : f) {	//Checks through all maps and images within base dir
+				
+				if(file != null && file.getName().toLowerCase().endsWith(".jpg") && file.isFile()){
+					try {
+						images.put( file.getName(), ImageIO.read( new File(file.getCanonicalPath())) );	//add file in base dir
+					}
+					catch (IOException e)
+					{
+						e.printStackTrace();
+					}
+					
+				}else if( !file.isFile() )
+					addFilesFromMap(file);
+			}
+		}
 	}
 	
-	public void initialize() {
+	/**
+	 * Add files from directory map. May reuse itself if there's a map within the map
+	 * @param dir
+	 */
+	public void addFilesFromMap(File map) {
+		
 		try {
-			File directory = new File("img/World/");
-			File[] f = directory.listFiles();
-			
-			if(directory.isDirectory()) {
-				
+			if(map.isDirectory()) {
+				File[] f = map.listFiles();
+
 				for(File file : f) {
-					
-					System.out.println("Absolute path:  " + file.getAbsolutePath() + "\nCanonical path: " + file.getCanonicalPath() + "\n");
-					
-					if(file != null && file.getName().toLowerCase().endsWith(".jpg"))
-					{
-						images.put( file.getName(), ImageIO.read( new File(file.getCanonicalPath())) );
+					if( !file.isFile() ) {
+						addFilesFromMap(file);
+						
+					}else if(file != null && file.getName().toLowerCase().endsWith(".jpg") && file.isFile()) {
+						images.put( file.getName(), ImageIO.read( new File(file.getCanonicalPath())) );	//Add files from map
 					}
 				}
 			}
@@ -45,6 +69,12 @@ public class ImageDatabase
 		
 	}
 	
+	/**
+	 * Return an image from hashmap of images
+	 * @param imgName
+	 * @return	if image exists, return that image
+	 * 			else return null
+	 */
 	public Image getImage(String imgName) {
 		return images.get(imgName);
 	}
