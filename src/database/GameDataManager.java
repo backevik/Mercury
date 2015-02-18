6191e03dbcfbab516fcb1ba4b71d55c165f6d508
DanneMite <danieledsinger@hotmail.com>
2015-02-18 19:13:03 +0100

    GameDatamanager 0.3.1
    Fixed with all databaseses

--- src/database/GameDataManager.java
+++ src/database/GameDataManager.java
@@ -1,44 +1,35 @@
 package database;
 
 import java.awt.Image;
-import java.util.ArrayList;
 import player.Quest;
 import character.Item;
 import character.Spell;
+import database.ItemDatabase;
 
 /**
  * @author		Daniel Edsinger 	<danieledsinger@hotmail.com>
- * @version		0.3
+ * @version		0.3.1
  * @since		2015-02-17
  */
 
 public class GameDataManager 
 {
-	
 	private static GameDataManager instance = null;
-	private ArrayList<DataState> states;
-	private int currentState;
 	
 	private QuestDatabase quests;
 	private ItemDatabase items;
 	private SpellDatabase spells;
 	private ImageDatabase images;
 	
-	public static final int ITEMS_STATE = 0;
-	public static final int QUEST_STATE = 1;
-	public static final int SPELL_STATE = 2;
+	public static final int QUEST_STATE = 0;
+	public static final int SPELL_STATE = 1;
 	
 	private GameDataManager(){
-		states = new ArrayList<DataState>();
-		items = new ItemDatabase();
+		
 		quests = new QuestDatabase();
 		spells = new SpellDatabase();
 		
-		states.add(items);
-		states.add(quests);
-		states.add(spells);
-		setState(0);
-		
+		items = new ItemDatabase();
 		images = new ImageDatabase();
 		
 	}
@@ -55,39 +46,6 @@ public class GameDataManager
 	}
 	
 	/**
-	 * saving and loading
-	 * The currentState variable is in charge of what to save and load
-	 */
-	public void save() {
-		states.get(currentState).save();
-	}
-	
-	public void load() {
-		states.get(currentState).load();
-	}
-	
-	public void setState(int state) {
-		currentState = state;
-	}
-	
-	public int getCurrentState() {
-		return currentState;
-	}
-	
-	
-	/**
-	 * Add objects to data
-	 */
-	public void addItem(Item item) {
-		items.addItem(item);
-	}
-	
-	public void addQuest(Quest quest) {
-		quests.addQuest(quest);
-	}
-	
-	
-	/**
 	 * Return lists of objects
 	 */
 	public java.util.List<Quest> getQuests() {
@@ -101,7 +59,6 @@ public class GameDataManager
 	public java.util.List<Spell> getSpells() {
 		return spells.getSpells();
 	}
-	
 	
 	public Image getImage(String imgName) {
 		return images.getImage(imgName);
