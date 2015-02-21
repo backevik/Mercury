93a11bc033c1ade29bcdb9985b6237a26efcfb7e
backevik <a.backevik@hotmail.com>
2015-02-21 22:12:11 +0100

    Version 0.4

--- src/combat/Enemy.java
+++ src/combat/Enemy.java
@@ -1,46 +1,15 @@
 package combat;
-
-import java.util.Random;
-
 import character.Character;
-import character.Spell;
 
 /**
- * @author      Andreas Bäckevik
+ * @author      Andreas BÃ¤ckevik
  * @version     0.1
  * @since       2015-02-01
  */
 
-public class Enemy extends Character
-{
-	private Random rand;
-	
+public class Enemy extends Character{
 	public Enemy (String name,int level) {
 		super(name);
 		this.setLevel(level);
-		rand = new Random();
-	}
-	
-	/**
-	 * Decides AI's next move.
-	 * @return whatToDo - String with command on next move. heal/attack/spell 
-	 */
-	public String AI(){
-		String whatToDo=null;
-		if(this.getValueOfVital("Health")<(this.getMaxOfVital("Health")/8)){
-			for(Spell spell : this.getSpellBook().getSpells()){
-				if(spell.getName().equals("heal")){
-					whatToDo="heal";
-				}
-			}
-		}else{
-			int atk = rand.nextInt(2)+1;
-			if(atk==1){
-				whatToDo="attack";
-			}else{
-				whatToDo="spell";
-			}
-		}
-		return whatToDo;
 	}
 }
