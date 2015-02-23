dcd88b3c39e46c937f03fb8c4e145c567bf558fd
backevik <a.backevik@hotmail.com>
2015-02-23 16:02:52 +0100

    Version 0.4 TOTAL DONE

--- src/combat/Enemy.java
+++ src/combat/Enemy.java
@@ -1,15 +1,19 @@
 package combat;
 import character.Character;
+import character.Spell;
 
 /**
- * @author      Andreas Bäckevik
+ * @author      Andreas BÃ¤ckevik
  * @version     0.1
  * @since       2015-02-01
  */
 
 public class Enemy extends Character{
-	public Enemy (String name,int level) {
+	public Enemy (String name,int level,Spell...spells) {
 		super(name);
+		for(Spell spell : spells){
+			this.getSpellBook().addSpell(spell);
+		}
 		this.setLevel(level);
 	}
 }
