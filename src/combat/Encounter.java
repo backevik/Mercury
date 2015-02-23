22af535423c7f9d01fd24254b7ab366ca35fd86c
backevik <a.backevik@hotmail.com>
2015-02-23 16:02:28 +0100

    Version 0.4 TOTAL DONE

--- src/combat/Encounter.java
+++ src/combat/Encounter.java
@@ -6,22 +6,20 @@ import java.util.List;
 import character.Spell;
 
 /**
- * @author      Andreas BÃ¤ckevik
+ * @author      Andreas BÃƒÂ¤ckevik
  * @version     0.4
  * @since       2015-02-20
  */
 
 public class Encounter {
-	private final List<Enemy> enemies = new ArrayList<>();
+	private final List<Enemy> enemyList = new ArrayList<>();
 	private String event;
 	
-	public Encounter(String event){
-		this.event = event;
-		Enemy enemy = new Enemy("Big evil tomato",1); //temp tills vi har en enemy database
-		enemy.getSpellBook().addSpell(new Spell("fireball","eld av boll",10,"damage",20));
-		enemy.getSpellBook().addSpell(new Spell("heal","helar dig",10,"heal",20));
-    	
-		enemies.add(enemy);
+	public Encounter(String event,Enemy...enemies){
+		for(Enemy enemy : enemies){
+			enemyList.add(enemy);
+		}
+		this.event = event;   	
 	}
 	
 	public String getEvent(){
@@ -29,6 +27,6 @@ public class Encounter {
 	}
 	
 	public List<Enemy> getEnemies(){
-		return enemies;
+		return enemyList;
 	}
 }
