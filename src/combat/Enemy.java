054879ec23ba19120bf7ee6a228d0d5f5a3404a3
backevik <a.backevik@hotmail.com>
2015-02-25 18:26:44 +0100

    Fixed enemy picture

--- src/combat/Enemy.java
+++ src/combat/Enemy.java
@@ -1,19 +1,30 @@
 package combat;
+import java.awt.Image;
+
+
+import zlibrary.ZImage;
 import character.Character;
 import character.Spell;
+import database.ImageDatabase;
 
 /**
- * @author      Andreas BÃ¤ckevik
+ * @author      Andreas BÃƒÂ¤ckevik
  * @version     0.1
  * @since       2015-02-01
  */
 
 public class Enemy extends Character{
-	public Enemy (String name,int level,Spell...spells) {
+	private Image img;
+	public Enemy (String name,int level,Image img,Spell...spells) {
 		super(name);
 		for(Spell spell : spells){
 			this.getSpellBook().addSpell(spell);
 		}
 		this.setLevel(level);
+		this.img = img;
+	}
+	
+	public Image getImage(){
+		return img;
 	}
 }
