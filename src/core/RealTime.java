e451fe2d618ce3f3feee5236678145dc62c067d3
Mattias Benngard <mbengan@gmail.com>
2015-03-08 00:29:04 +0100

    Officielt klar okej.

--- src/core/RealTime.java
+++ src/core/RealTime.java
@@ -1,12 +1,11 @@
 package core;
 
 /**
- * @author	Anton Andrén & Mattias Benngård
- * @version	0.4
+ * Interface for all objects that need real-time updates.
+ * 
+ * @author	Anton Andrén
+ * @version	1.0					<2015-03-08>
  * @since	2015-02-16
- *
- * Interface for all objects that need real-time updates
- * All classes that implement Entity must implement public void update()
  */
 
 public interface RealTime
